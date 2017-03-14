e8bd751b9826ac2c7358cfa00ff72f7655023076
Daisy Ndungu <daisywndungu@gmail.com>
2017-03-14 08:02:26 +0300

    Add a function to create room(s)

--- amity/amity.py
+++ amity/amity.py
@@ -23,3 +23,25 @@ class Amity(object):
         self.new_room_name = ''
         self.room_type = ''
         self.room_names = []
+
+    def create_room(room_type, *room_names):
+        if room_type == 'o' or room_type == 'O':
+            for room_name in room_names:
+                if room_name in self.all_rooms['office']:
+                    print("!!!!!!........%s room already exists.........!!!!!!" % room_name)
+                else:
+                    self.all_rooms = self.all_rooms['office'].append(room_name)
+                    print("<<........An Office: %s has been created.........>>" % room_name)
+
+        elif room_type == 'l' or room_type == 'l':
+            for room_name in room_names:
+                if room_name in self.all_rooms['living_space']:
+                    print("!!!!!!........%s already exists.........!!!!!!" % room_name)
+                else:
+                    self.all_rooms['living_space'].append(room_name)
+                    print("<<........A Living Space: %s has been created.........>>"% room_name)
+            print(self.all_rooms['living_space'])
+        else:
+            print("invalid input. Type should be 'o' for office(s) or 'l' for living_space(s)")
+
+    
